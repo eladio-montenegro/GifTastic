f07f5b353b36f379ead7aff015a8c846097e7163
Eladio Montenegro <eladio.montenegro@gmail.com>
2019-07-18 18:22:08 -0500

    added the guiphy api key to my js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+//initial array of state
+var states = ["Illinois", "California", "Indiana", "Florida", "New York"];
+
+// displayMovieInfo function re-renders the HTML to display the appropriate content
+function displayStatesInfo() {
+
+    var states = $(this).attr("data-name");
+    var queryURL = "https://www.https://api.giphy.com/v1/.com/?t=" + states + "&apikey=1OtdAX8hHrdlbBxQm3zjcoBL390pxYW4";}
