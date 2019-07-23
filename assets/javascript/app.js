a3f48fae5bf0d19a7fbf2446f6d5375b63c7b8ed
Eladio Montenegro <eladio.montenegro@gmail.com>
2019-07-22 23:47:35 -0500

    got the api to work, but only in the console log.  Still have to display the results on the page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 //initial array of state
 var states = ["Illinois", "New York", "California", "Texas", "Florida" ];
 
+//create buttons for the states in the array
 $(document).ready(function() {
     renderButton();
     function renderButton() {
@@ -18,6 +19,7 @@ $(document).ready(function() {
         }
 
     }
+    //add a button after someone enter a new name into the form field
     $("#addbutton").on("click", function(event) {
         event.preventDefault();
         var addedData = $("#userinput").val().trim();
@@ -27,10 +29,21 @@ $(document).ready(function() {
             $("#userinput").val(" ");
         }
 });
-// displayMovieInfo function re-renders the HTML to display the appropriate content
-// function displayStatesInfo() {
+//pull info from the API and show the giff
+$(document).on("click", ".itembutton", displayInfo);
 
-//     var states = $(this).attr("data-name");
-//     var queryURL = "https://www.https://api.giphy.com/v1/.com/?t=" + states + "&apikey=1OtdAX8hHrdlbBxQm3zjcoBL390pxYW4&limit=10";}
+function displayInfo() {
+    var states = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    states + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    $("mainimages").empty();
 
-})
+    $.ajax ({
+        url: queryURL,
+        method: "GET"
+    }) .then(function(response) {
+        console.log(response);
+    
+    });
+}
+});
