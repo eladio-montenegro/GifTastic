21128377b7c7a3361077498239d7d60b20fda4bc
Eladio Montenegro <eladio.montenegro@gmail.com>
2019-07-22 22:26:01 -0500

    got the array of button to show up on the page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,26 @@
 //initial array of state
-var states = ["Illinois", "California", "Indiana", "Florida", "New York"];
+var states = ["Illinois", "New York", "California", "Texas", "Florida" ];
 
+$(document).ready(function() {
+    renderButton();
+    function renderButton() {
+        $("#allbuttons").empty();
+
+        for (var i=0 ; i < states.length; i++) {
+
+            var newButton = $("<button>");
+            newButton.addClass("itembutton");
+            newButton.addClass("btn btn-success");
+            newButton.text(states[i]);
+            newButton.attr("data-name", states[i]);
+            $("#allbuttons").append(newButton);
+
+        }
+
+    }
+})
 // displayMovieInfo function re-renders the HTML to display the appropriate content
-function displayStatesInfo() {
+// function displayStatesInfo() {
 
-    var states = $(this).attr("data-name");
-    var queryURL = "https://www.https://api.giphy.com/v1/.com/?t=" + states + "&apikey=1OtdAX8hHrdlbBxQm3zjcoBL390pxYW4";}
+//     var states = $(this).attr("data-name");
+//     var queryURL = "https://www.https://api.giphy.com/v1/.com/?t=" + states + "&apikey=1OtdAX8hHrdlbBxQm3zjcoBL390pxYW4&limit=10";}
